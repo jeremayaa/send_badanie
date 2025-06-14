d3802b6470fca37290ffee88990241bc5c304c25
Jeremiasz <jeremiasz@MacBook-Air-Jeremiasz.local>
2025-06-14 14:36:19 +0200

    paginacja, lepsze sortowanie badan

--- send_badanie/settings.py
+++ send_badanie/settings.py
@@ -48,6 +48,8 @@ REST_FRAMEWORK = {
     'django_filters.rest_framework.DjangoFilterBackend',
     'rest_framework.filters.OrderingFilter',
   ],
+  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+  'PAGE_SIZE': 5,
 }
 
 MIDDLEWARE = [
